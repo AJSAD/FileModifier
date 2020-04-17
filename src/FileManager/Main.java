af52b9cf5e2e1a028fb07354446169fa027544dd
Ajay Daniel <ajaydaniel001@gmail.com>
2020-04-17 19:58:46 +0530

    Basic Start Structure
    ready to start

---
+++ src/FileManager/Main.java
@@ -0,0 +1,8 @@
+package FileManager;
+
+public class Main {
+	public static void main( String args[] )
+	{
+		System.out.println( "TO BE STARTED SOON" );
+	}
+}
--- test/README.md
+++
@@ -1,2 +0,0 @@
-# FileManipulator
-To maintain 
